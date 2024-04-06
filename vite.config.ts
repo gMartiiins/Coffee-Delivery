5f9f9cf41f7b730260c89fa982b6d9ac1c0ee6ff
gMartiiins <gmartiiins0@gmail.com>
2024-04-06 12:38:03 -0300

    Construir site para implantação no GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Coffee-Delivery"
+  base: "/",
+  build: {
+    outDir: 'dist' // Configura a pasta de saída para 'dist'
+  }
 })
